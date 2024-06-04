685ccd8284e30b66470da14a6822b9226c5deec1
Denis11m <denissmi87@gmail.com>
2024-06-04 19:48:56 +0200

    adding logic of human choice

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-let choice = ['rock', 'paper', 'scissors'];
+let choice = ["rock", "paper", "scissors"];
 
+// function getComputerChoice() {
+//     for (let i = 0; i < choice.length; i++){
+//         return Math.floor(Math.random() * choice.length);
+//         getComputerChoice.toString();
+//     }
+
+// }
+
+// Write the logic to get the computer choice
 
 function getComputerChoice() {
-    for (let i = 0; i < choice.length; i++){
-        return Math.floor(Math.random() * choice.length);
-    }
-    
+    let randomIndex = Math.floor(Math.random() * choice.length);
+
+    return choice[randomIndex];
 }
+// console.log(getComputerChoice());
+
+
+// Write the logic to get the human choice
+
+function getHumanChoice() {
+    choice = prompt("please enter your choice?");
+    if (choice === "rock") {
+        console.log("rock");
+        return choice;
+    } else if (choice === "paper") {
+        console.log("paper");
+        return choice;
+    } else if (choice === "scissors") {
+        console.log("scissors");
+        return choice;
+    } else {
+        console.log("No Vaild Parameter!");
+    }
+}
+
+getHumanChoice();
+
+
+// Declare the players score variables
+
